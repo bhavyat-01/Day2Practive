518f29c740f19b1560fbbd8910a594088760024d
bhavyat-01 <bhavya.tanugula@gmail.com>
2023-11-13 12:29:21 -0500

    Implemented Timer

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ public class Main {
     static int sum = 0;
 
     public static void main(String args[]) throws ExecutionException, InterruptedException {
+        long startTime = System.currentTimeMillis();
+
         CallableFile[] ourCalls = new CallableFile[100];
 
         for(int i=0; i<100; i++) {
@@ -18,7 +20,11 @@ public class Main {
             sum+=future.get();
         }
 
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+
         System.out.println(sum);
+        System.out.println("Total Time Taken: "+totalTime+" ms");
 
     }
 }
