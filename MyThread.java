e684674852c3bc9728db5aae03c782964a3a50ff
bhavyat-01 <bhavya.tanugula@gmail.com>
2023-11-13 12:06:07 -0500

    Counting to 1 million

--- MyThread.java
+++ MyThread.java
@@ -1,2 +1,15 @@
-public class MyThread {
+import java.util.concurrent.Callable;
+
+public class MyThread implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        //in each thread count up to 1 mil
+        int count = 0;
+
+        for(int i = 0; i<1000000; i++){
+            count++;
+        }
+
+        return count;
+    }
 }
